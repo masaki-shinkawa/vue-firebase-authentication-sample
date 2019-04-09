b3aa2b0a1d15b234b0b507856e82d4b9143811eb
Masaki Shinkawa <masaki.shinkawa@kdl.co.jp>
2019-04-09 11:39:41 +0900

    first-commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 追加ここから ---
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+var config = {
+  apiKey: "AIzaSyAsbEOjgm12wwwMFHg-V6KqmYXtd6YxLUY",
+  authDomain: "fb-auth-test-1111.firebaseapp.com",
+  databaseURL: "https://fb-auth-test-1111.firebaseio.com",
+  projectId: "fb-auth-test-1111",
+  storageBucket: "fb-auth-test-1111.appspot.com",
+  messagingSenderId: "693208013602"
+};
+firebase.initializeApp(config);
+// 追加ここまで ---
 
 Vue.config.productionTip = false
 
